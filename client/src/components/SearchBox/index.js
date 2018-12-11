9d51b3f5457d204a5a4253ab2e47eced1a74fe11
Juan Martín Pithod <juantincho@gmail.com>
2018-12-11 01:05:57 -0300

    Add react-router, navigation in app and SearchBox

--- client/src/components/SearchBox/index.js
+++ client/src/components/SearchBox/index.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import logo from '../../images/LOGO_ML.png';
 import './search.scss';
 
 class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      textValue: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ textValue: event.target.value });
+  };
+
+  handleKeyPress = (e) => {
+    if (e.which === 13) {
+      this.searchItem();
+    }
+  };
+
+  searchItem = () => {
+    const { textValue } = this.state;
+    const { history } = this.props;
+    history.push(`/items?search=${textValue}`);
+  };
+
   render() {
+    const { textValue } = this.state;
     return (
-      <div>
-        <input />
-        <button />
+      <div className="search">
+        <div className="column1">
+          <img src={logo} alt="ML-logo" />
+        </div>
+        <div className="column2">
+          <input
+            value={textValue}
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+            placeholder="Nunca dejes de buscar"
+          />
+          <button type="submit" onClick={this.searchItem} />
+        </div>
       </div>
     );
   }
@@ -15,4 +52,4 @@ class SearchBox extends Component {
 
 SearchBox.propTypes = {};
 
-export default SearchBox;
+export default withRouter(SearchBox);
--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductsList from '../../components/ProductsList';
 import SearchBox from '../../components/SearchBox';
+import './app.scss';
 
-const App = props => <SearchBox />;
-
-App.propTypes = {};
+const App = () => (
+  <Router>
+    <div className="app">
+      <div className="header">
+        <SearchBox />
+      </div>
+      <Switch>
+        <Route path="/items/:id" component="" />
+        <Route path="/items" component={ProductsList} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
